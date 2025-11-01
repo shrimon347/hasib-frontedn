fff88cd43e22a844ddb88ba7067b31719252396f
shrimon347 <shrimon1999@gmail.com>
2025-11-01 20:36:58 +0600

    fix issue

--- hooks/useLenis.ts
+++ hooks/useLenis.ts
@@ -1,28 +1,27 @@
 "use client";
 
-import { useEffect } from "react";
 import Lenis from "@studio-freight/lenis";
+import { useEffect } from "react";
 
 export const useLenis = () => {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,            // smoothness
-      easing: (t: number) => t,  // linear easing
-      orientation: "vertical",   // scroll direction
-      gestureOrientation: "vertical",
-      smoothWheel: true,         // enable smooth wheel
-      smoothTouch: true,         // enable smooth touch
-    });
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.2, // smoothness
+            easing: (t: number) => t, // linear easing
+            orientation: "vertical", // scroll direction
+            gestureOrientation: "vertical",
+            smooth: true, // use `smooth` instead of smoothWheel/smoothTouch
+        });
 
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
 
-    requestAnimationFrame(raf);
+        requestAnimationFrame(raf);
 
-    return () => {
-      lenis.destroy(); // cleanup
-    };
-  }, []);
+        return () => {
+            lenis.destroy(); // cleanup
+        };
+    }, []);
 };
