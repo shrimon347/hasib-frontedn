c2652a5f15ecfed90f7d61063a6410fd13a30156
shrimon347 <shrimon1999@gmail.com>
2025-11-01 20:54:26 +0600

    fix issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import AboutSection from "@/components/sections/AboutSection";
 import CaseSection from "@/components/sections/CaseSection";
 import { ContactSection } from "@/components/sections/ContactSection";
@@ -9,22 +10,32 @@ import ProcessSection from "@/components/sections/ProcessSection";
 import ServicesSection from "@/components/sections/ServicesSection";
 import SkillsTabs from "@/components/sections/SkillsTabs";
 import Testimonials from "@/components/sections/Testimonials";
-import { useLenis } from "@/hooks/useLenis";
+
+import { ReactLenis } from "lenis/react";
 
 export default function Home() {
-    useLenis();
     return (
-        <div className="space-y-20 md:space-y-32">
-            <HeroSection />
-            <AboutSection />
-            <ExperienceSection />
-            <ServicesSection />
-            <ProcessSection />
-            <SkillsTabs />
-            <Portfolio />
-            <CaseSection />
-            <ContactSection />
-            <Testimonials />
-        </div>
+        <ReactLenis
+            root
+            options={{
+                duration: 1.5,
+                easing: (t) => t,
+                orientation: "vertical",
+                gestureOrientation: "vertical",
+            }}
+        >
+            <div className="space-y-20 md:space-y-32">
+                <HeroSection />
+                <AboutSection />
+                <ExperienceSection />
+                <ServicesSection />
+                <ProcessSection />
+                <SkillsTabs />
+                <Portfolio />
+                <CaseSection />
+                <ContactSection />
+                <Testimonials />
+            </div>
+        </ReactLenis>
     );
 }
--- hooks/useLenis.ts
+++ hooks/useLenis.ts
@@ -1,28 +1,9 @@
 "use client";
 
-import Lenis from "@studio-freight/lenis";
-import { useEffect } from "react";
+import { useLenis } from "lenis/react";
 
-export const useLenis = () => {
-    useEffect(() => {
-        const lenis = new Lenis({
-            duration: 1.2, // smoothness
-            easing: (t: number) => t, // linear easing
-            orientation: "vertical", // scroll direction
-            gestureOrientation: "vertical",
-            smoothWheel: true, // enable smooth wheel
-            smoothTouch: true, // enable smooth touch
-        });
+export const useSmoothScroll = () => {
+    const lenis = useLenis((lenis) => {});
 
-        function raf(time: number) {
-            lenis.raf(time);
-            requestAnimationFrame(raf);
-        }
-
-        requestAnimationFrame(raf);
-
-        return () => {
-            lenis.destroy(); // cleanup
-        };
-    }, []);
+    return lenis;
 };
